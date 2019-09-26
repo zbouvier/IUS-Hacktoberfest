854ef562b600620c1efedc75a49d6407189687dc
Zach Bouvier <zbouvier99@gmail.com>
2019-09-26 18:20:08 -0400

    skeleton for data

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # IUS-Hacktoberfest
+
 A collection of data points and a data visualization model for the 2019 Hacktoberfest at IUS
 
 ## How do I get my name added
+
 The format for your new addition should look like this: (TODO)
---
+++ dataPresenter.py
@@ -0,0 +1,30 @@
+import plotly.graph_objects as go
+import json
+
+def parseLabels():
+    with open('data.json') as json_file:
+        data = json.load(json_file)
+
+    for p in data['people']:
+        userLabel = p['year']
+        if(userLabel == ''):
+            userLabel = 'Professor'
+        if(userLabel not in labels):
+            labels.append(userLabel)
+            values.append(1)
+        else:
+            values[labels.index(userLabel)] = values[labels.index(userLabel)] + 1
+    print(labels)
+    print(values)
+
+    
+        
+
+
+    
+labels = []
+values = []
+parseLabels()
+fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+fig.show()
